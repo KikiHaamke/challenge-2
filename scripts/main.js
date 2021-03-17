96ca1c9208124fa21325558907ac6e281ea5cee4
KikiHaamke <79255027+KikiHaamke@users.noreply.github.com>
2021-03-17 13:45:42 +0100

    Fases vd dag ingesteld

--- scripts/main.js
+++ scripts/main.js
@@ -17,19 +17,19 @@ function showTime(){
 function setDayPart(hours) {
     if (hours < 6) {
         // good night
-        document.body.className = 'night';
-        document.getElementById('message').innerText = 'Good night!';
+        document.getElementById('graphic').className = 'night';
+        document.getElementById('message').innerText = 'Good night! Time to go to bed.';
     } else if (hours < 12) {
         // good morning
-        document.body.className = 'morning';
-        document.getElementById('message').innerText = 'Good morning!';
+        document.getElementById('graphic').className = 'morning';
+        document.getElementById('message').innerText = 'Good morning! Time for breakfast.';
     } else if (hours < 18) {
         // good day
-        document.body.className = 'day';
-        document.getElementById('message').innerText = 'Good day!';
+        document.getElementById('graphic').className = 'day';
+        document.getElementById('message').innerText = 'Good day! It is a beautiful day today.';
     } else {
         // good evening
-        document.body.className = 'evening';
-        document.getElementById('message').innerText = 'Good evening! It\'s movie night tonight!';
+        document.getElementById('graphic').className = 'evening';
+        document.getElementById('message').innerText = 'Good evening! Movie night!';
     }
 }
